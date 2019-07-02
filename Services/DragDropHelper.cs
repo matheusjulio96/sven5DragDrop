66f1065d1034962e9f5d48036d621aac953ec621
matheusjulio96 <matheus.julio96@hotmail.com>
2019-07-02 09:48:41 -0300

    Add project files.

---
+++ Services/DragDropHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sven5DragDrop.Services
+{
+    public class DragDropHelper
+    {
+        public DragItem Item { get; set; }
+
+        public DragItem[] Items = new[]
+        {
+            new DragItem
+            {
+                Text = "Drag Item 1",
+                Description = "foo",
+                Index  = 1
+            },
+            new DragItem
+            {
+                Text = "Drag Item 2",
+                Description = "bar",
+                Index  = 1
+            }
+        };
+    }
+}
---
+++ Services/DragItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sven5DragDrop.Services
+{
+    public class DragItem
+    {
+        public string Text { get; set; }
+
+        public string Description { get; set; }
+        public int Index { get; set; }
+    }
+}
